568fd2dee8f9256e1dbfda7a4576de1bcae5c83a
simonleandergrimm <simonleandergrimm@gmail.com>
2025-04-11 13:35:50 +0000

    Consolidated blast and ww specific scripts into interpret-blast.py and blast-ww.py

--- scripts/blast.py
+++ scripts/blast.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
+# Script to BLAST reads against a set of genomes, separately returning reads that are accounted for with a good match and those that are not. This script is used in conjunction with scripts/interpret-blast.py, like so:
+# 1. Run interpret-blast.py on to_validate_*.fasta to understand which genome in the online BLAST results has the highest cumulative bit score acorss all reads.
+# 2. Add that genome to ww-genomes.json or swabs-genomes.json, depending on which set of reads you're analyzing.
+# 3. Run blast.py, returning the reads that did not show a good match to said genome (`*-not-accounted-for.fasta`).
+# 4. Run interpret-blast.py on `*-not-accounted-for.fasta`, again identifying the genome that now best matches the remaining reads.
+# 5. Again, add genome to `*-genomes.json`, and repeat the loop using `*-not-accounted-for.fasta` until you hit diminishing returns.
+
 import os
 import csv
 import glob
 import hashlib
 import subprocess
+import argparse
+import json
 from collections import defaultdict, Counter
 from Bio.SeqIO.FastaIO import SimpleFastaParser
 
@@ -13,34 +22,17 @@ work_dir = "validation-work"
 os.makedirs(output_dir, exist_ok=True)
 os.makedirs(work_dir, exist_ok=True)
 
-genome_names = {
-    'MW587042.1': 'Human coronavirus OC43',
-    'OR833061.1': 'Human coronavirus NL63',
-    'DQ415901.1': 'Human coronavirus HKU1',
-    'PQ243243.1': 'Human coronavirus 229E',
+# After first using a score of 200, reads mapping to a viruses added to `*-genomes.json` still ended up in the `*-not-accounted-for.fasta`. Many of these reads were short, but did actually have a best match to said genome witha score under 200. Lowering the threshold to 100 fixed this.
+BLAST_SCORE_THRESHOLD = 100
 
-    'PV178553.1': 'Rhinovirus A34',
-    'OK649392.1': 'Rhinovirus A94',
+parser = argparse.ArgumentParser()
+parser.add_argument("--label", type=str, choices=["swabs", "ww"], required=True)
+args = parser.parse_args()
+label = args.label
 
-    'PV178233.1': 'Rhinovirus B37',
-
-    'PV178199.1': 'Rhinovirus C2',
-    'MZ670596.1': 'Rhinovirus C36',
-    'PQ602524.1': 'Rhinovirus C42',
-    'MW969528.1': 'Rhinovirus C56',
-
-    'PQ376588.1': 'RSVA',
-    'PV206811.1': 'RSVB',
-
-    'NC_012485.1': 'Human papillomavirus type 109',
-    'KR816181.1': 'Human papillomavirus type 194',
-
-    'PQ800221.1': 'HPIV4',
-}
-
+with open(os.path.join(work_dir, "%s-genomes.json" % args.label), "r") as f:
+    genome_names = json.load(f)
 genomes = sorted(genome_names)
-read_tsv_gz = os.path.join(work_dir, "to_validate_swabs.tsv")
-reads_fasta = os.path.join(work_dir, "to_blast_swabs.fasta")
 
 read_dates = {}
 read_locs = {}
@@ -48,7 +40,9 @@ read_seqs = {}
 read_taxids = {}
 read_samples = {}
 
-with open(read_tsv_gz, "rt") as inf, \
+reads_fasta = os.path.join(work_dir, "to_blast_%s.fasta" % label)
+
+with open(os.path.join(work_dir, "to_validate_%s.tsv" % label), "rt") as inf, \
      open(reads_fasta, "w") as outf:
     for row in csv.DictReader(inf, delimiter='\t'):
         outf.write(">%s\n%s\n" % (row["read_id"], row["sequence"]))
@@ -57,6 +51,7 @@ with open(read_tsv_gz, "rt") as inf, \
         read_seqs[row["read_id"]] = row["sequence"]
         read_taxids[row["read_id"]] = int(row["taxid"])
         read_samples[row["read_id"]] = row["sample_id"]
+
 
 taxid_names = {}
 with open("index/20250314.taxonomy-names.dmp") as inf:
@@ -82,17 +77,19 @@ if not any(glob.glob("%s*" % blastdb)):
         for genome in genomes:
             genome_fname = os.path.join(genome_download_dir, "%s.fasta" % genome)
             if not os.path.exists(genome_fname):
+                print("Downloading %s" % genome)
                 subprocess.check_call(["scripts/download_fasta.sh", genome, genome_download_dir])
             with open(genome_fname) as inf:
                 for title, seq in SimpleFastaParser(inf):
                     outf.write(">%s\n%s\n" % (title, seq))
-
 
     subprocess.check_call([
         "makeblastdb",
         "-in", allgenomes_fname,
         "-dbtype", "nucl",
         "-out", blastdb])
+
+
 
 blast_results = os.path.join("%s.blast" % reads_fasta)
 
@@ -103,9 +100,10 @@ subprocess.check_call([
     "-out", blast_results,
     "-outfmt", "6"])
 
+# First, identify primary genomes for each read
+
 good_primary_hits = {}
 
-# First, identify primary genomes for each read
 with open(blast_results) as inf:
     for line in inf:
         (read_id, genome, pct_identity, alignment_length,
@@ -115,7 +113,7 @@ with open(blast_results) as inf:
 
         bit_score = float(bit_score)
 
-        if bit_score < 100:
+        if bit_score < BLAST_SCORE_THRESHOLD:
             continue
 
         if (read_id not in good_primary_hits or
@@ -131,7 +129,6 @@ past_observations = {} # (date, loc, genome) -> [(start, end)]
 WIGGLE_ROOM=3 # count dups even if start/end is off by this many bp
 
 dedup_entries = []
-
 with open(blast_results) as inf:
     for line in inf:
         (read_id, genome, pct_identity, alignment_length,
@@ -152,7 +149,7 @@ with open(blast_results) as inf:
         key = read_dates[read_id], read_locs[read_id], genome
         if key not in past_observations:
             past_observations[key] = []
-            dedup_observations[key] = []
+            dedup_observations[key] = [] # Note, might we want to have the key be the underlying sample? That way we differentiate between MJ and NAO samples.
 
         genome_start = min(int(genome_start_raw), int(genome_end_raw))
         genome_end = max(int(genome_start_raw), int(genome_end_raw))
@@ -160,7 +157,7 @@ with open(blast_results) as inf:
 
         is_duplicate = False
 
-        # Check if the new observation is within WIGGLE_ROOM bp of anyexisting observation.
+        # Check if the new observation is within WIGGLE_ROOM bp of any existing observation.
         for existing_start, existing_end in past_observations[key]:
             if (abs(existing_start - genome_start) <= WIGGLE_ROOM and
                 abs(existing_end - genome_end) <= WIGGLE_ROOM):
@@ -169,7 +166,6 @@ with open(blast_results) as inf:
         if not is_duplicate:
             dedup_observations[key].append((
                 read_id, genome_start, genome_end))
-
             dedup_entries.append([
                 read_id,
                 read_dates[read_id],
@@ -177,18 +173,19 @@ with open(blast_results) as inf:
                 read_samples[read_id],
                 genome,
                 genome_names[genome],
+                read_taxids[read_id],
                 genome_start,
                 genome_end,
                 bit_score,
             ])
         past_observations[key].append((genome_start, genome_end))
 
-
 # ============================================================================
 #                                 OUTPUT
 # ============================================================================
 
-with open(os.path.join(output_dir, "swabs-classified-dedup-reads.tsv"), "w") as outf:
+
+with open(os.path.join(output_dir, "%s-classified-dedup-reads.tsv" % label), "w") as outf:
     # Write header
     header = ["read_id", "date", "loc", "sample", "genome", "genome_name", "taxid", "start", "end", "bit_score"]
     outf.write("\t".join(header) + "\n")
@@ -202,82 +199,34 @@ for read_id in read_dates:
     if read_id not in good_primary_hits:
         missing.add(read_id)
 
-with open(os.path.join(work_dir, "swabs-not-accounted-for.fasta"), "w") as outf:
+with open(os.path.join(work_dir, "%s-not-accounted-for.fasta" % label), "w") as outf:
     for read_id in sorted(missing):
         outf.write(">%s\n%s\n" % (read_id, read_seqs[read_id]))
 
-# ============================================================================
-#                                 VALIDATION
-# ============================================================================
-
-
-if False:
-    genome_counts = Counter()
-    for read_id, (read_genome, _) in good_primary_hits.items():
-        genome_counts[read_genome] += 1
-    for count, genome in sorted((c,g) for (g, c) in genome_counts.items()):
-        print(count, genome, genome_names[genome], sep="\t")
-
-    for genome in genomes:
-        if genome not in genome_counts:
-            print("redundant", genome)
-
-if False:
-    genome_counts = Counter()
-    genomes_for_name = defaultdict(set)
-    for read_id, (read_genome, _) in good_primary_hits.items():
-        genome_counts[genome_names[read_genome]] += 1
-        genomes_for_name[genome_names[read_genome]].add(read_genome)
-
-    date_locs = sorted(set((read_dates[read_id], read_locs[read_id])
-                           for read_id in read_locs))
-    date_loc_strs = ["%s-%s" % dl for dl in date_locs]
-
-    print("virus", "total_w_dups", "total_unique",
-          *date_loc_strs, sep="\t")
-    for count, genome_name in sorted((c,g) for (g, c) in genome_counts.items()):
-
-        date_loc_obs = []
-        for date, loc in date_locs:
-            obs_read_ids = set()
-            for (obs_date,
-                 obs_loc,
-                 obs_genome), obs_list in dedup_observations.items():
-                if (obs_genome not in genomes_for_name[genome_name] or
-                    obs_date != date or
-                    obs_loc != loc):
-                    continue
-                for read_id, _, _ in obs_list:
-                    obs_read_ids.add(read_id)
-            date_loc_obs.append(len(obs_read_ids))
-
-        print(genome_name,
-              count,
-              sum(date_loc_obs),
-              *date_loc_obs, sep="\t")
-
-
-# Check which Minimap2 taxids are missing
-missing_taxids = Counter()
-for read_id in missing:
-    missing_taxids[read_taxids[read_id]] += 1
-
-if True:
-    for count, taxid in sorted((c,t) for (t,c) in missing_taxids.items()):
-        print(count, taxid, taxid_names[taxid])
-
-if missing and False:
-    print("Failed to find genomes for:")
-    for read_id in sorted(missing):
-        print("%s %s" % (read_dates[read_id], read_locs[read_id]))
-        print(read_seqs[read_id])
-
-n_identified = len(good_primary_hits)
-n_total = len(read_dates)
 
 # ============================================================================
 #                                 SUMMARY
 # ============================================================================
+
+print("Genomes identified:")
+genome_counts = Counter()
+for read_id, (read_genome, _) in good_primary_hits.items():
+    genome_counts[read_genome] += 1
+for count, genome in sorted((c,g) for (g, c) in genome_counts.items()):
+    print(count, genome, genome_names[genome], sep="\t")
+
+# Check which taxids are missing
+missing_taxids = Counter()
+for read_id in missing:
+    missing_taxids[read_taxids[read_id]] += 1
+
+print("Taxids missing:")
+for count, taxid in sorted((c,t) for (t,c) in missing_taxids.items()):
+    print(count, taxid, taxid_names[taxid])
+
+n_identified = len(good_primary_hits)
+n_total = len(read_dates)
+
 
 print()
 print("Identified %s of %s (%.0f%%)" % (
--- scripts/interpret-blast.py
+++ scripts/interpret-blast.py
@@ -1,22 +1,48 @@
 #!/usr/bin/env python3
 
+# Script to show which genomes in online BLAST results has the highest cumulative bit score across all BLASTed reads. This script is used in conjunction with scripts/blast.py, like so:
+# 1. Run interpret-blast.py on to_validate_*.fasta to understand which genome in the online BLAST results has the highest cumulative bit score across all reads.
+# 2. Add that genome to ww-genomes.json or swabs-genomes.json, depending on which set of reads you're analyzing.
+# 3. Run blast.py, returning the reads that did not show a good match to said genome (`*-not-accounted-for.fasta`).
+# 4. Run interpret-blast.py on `*-not-accounted-for.fasta`, again identifying the genome that now best matches the remaining reads.
+# 5. Again, add genome to `*-genomes.json`, and repeat the loop using `*-not-accounted-for.fasta` until you hit diminishing returns.
+
 
 import json
 import glob
 from collections import Counter, defaultdict
+import argparse
 from Bio.SeqIO.FastaIO import SimpleFastaParser
 
 N_PRINT = 30
 
-target_deliveries = [
-    "NAO-BCL-2025-03-03",
-    "MJ-2025-01-20-a",
-    "MJ-2025-01-20-b",
-    "MJ-2025-03-01",
-    "MJ-2025-02-12",
-]
-# First, run scripts/summarize_validation_files.py. After first run of blast-ww.py, change to validation-work/ww-not-accounted-for.fasta
-limit_to = f"validation-work/to_validate_ww.fasta"
+parser = argparse.ArgumentParser()
+parser.add_argument("--label", type=str, choices=["swabs", "ww"], required=True)
+args = parser.parse_args()
+
+# First, run scripts/summarize_validation_files.py on to_validate_{args.label}.fasta. After first run of blast.py, change to validation-work/{args.label}-not-accounted-for.fasta
+limit_to = f"validation-work/to_validate_{args.label}.fasta"
+
+
+if args.label == "swabs":
+    target_deliveries = [
+        "NAO-ONT-20250120-Zephyr8",
+        "NAO-ONT-20250127-Zephyr9",
+        "NAO-ONT-20250213-Zephyr10",
+        "NAO-ONT-20250213-Zephyr10-QC",
+        "NAO-ONT-20250220-Zephyr11",
+        "NAO-ONT-20250226-Zephyr10-QC2",
+        "NAO-ONT-20250313-Zephyr12",
+    ]
+elif args.label == "ww":
+    target_deliveries = [
+        "NAO-BCL-2025-03-03",
+        "MJ-2025-01-20-a",
+        "MJ-2025-01-20-b",
+        "MJ-2025-03-01",
+        "MJ-2025-02-12",
+    ]
+
 
 limit_read_ids = set()
 with open(limit_to) as inf:
@@ -35,11 +61,12 @@ with open(limit_to) as inf:
         read_id = read_id.split("::")[0]
         read_seqs[read_id] = seq
 
+
 for delivery in target_deliveries:
     for blast_file in glob.glob(f"delivery_analyses/{delivery}/*Alignment.json"):
         with open(blast_file) as inf:
             blast_results = json.load(inf)
-        for result in blast_results["BlastOutput2"]:
+        for result in blast_results.get("BlastOutput2") or blast_results.get("blastOutput2", []):
             query_title = result["report"]["results"]["search"]["query_title"]
             query_title = query_title.split("::")[0]
             if query_title not in limit_read_ids:
@@ -50,8 +77,10 @@ for delivery in target_deliveries:
                     for hsp in hit["hsps"]:
                         try:
                             accession = description["accession"]
+
                             title = description["title"]
                             bit_score = hsp["bit_score"]
+
                             title_bit_score_sums[(accession, title)] += bit_score
                         except KeyError:
                             print(f"No further info about entry with gid {description['id']}: {description}")
