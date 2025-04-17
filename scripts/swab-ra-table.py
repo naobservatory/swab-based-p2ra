bd10a60722172bcd518cbbd1e13f00571b6259e1
simonleandergrimm <simonleandergrimm@gmail.com>
2025-04-17 15:20:51 +0000

    Updated ra table with fixed read counts and second table for treatment level metrics.

--- scripts/swab-ra-table.py
+++ scripts/swab-ra-table.py
@@ -9,22 +9,56 @@ from datetime import datetime
 from taxonomy import load_taxonomy_names
 
 # Constants and paths
-dashboard_dir = os.path.expanduser("~/code/mgs-restricted/dashboard")
 validation_output_dir = "validation-output"
 START_DATE = datetime(2025, 1, 7)
 END_DATE = datetime(2025, 2, 25)
+
+target_deliveries = [
+    "NAO-ONT-20250120-Zephyr8",
+    "NAO-ONT-20250127-Zephyr9",
+    "NAO-ONT-20250213-Zephyr10",
+    "NAO-ONT-20250213-Zephyr10-QC",
+    "NAO-ONT-20250220-Zephyr11",
+    "NAO-ONT-20250226-Zephyr10-QC2",
+    "NAO-ONT-20250313-Zephyr12",
+    "NAO-ONT-20250328-Zephyr12b"
+]
 
 # Functions
 def is_date_in_range(date):
     return START_DATE <= date <= END_DATE
 
-# Load data
+# Load names
 taxid_names = load_taxonomy_names()
 
+# Load counts
 read_counts = {}
-with open("n_reads_per_swab_sample.tsv") as f:
+with open(os.path.join("outputs", "n_reads_per_swab_sample.tsv")) as f:
     for row in csv.DictReader(f, delimiter="\t"):
         read_counts[row["sample"]] = int(row["reads"])
+
+
+# Create per-category counts and get treatment for each sample
+date_loc_read_counts = defaultdict(int)
+treatment_read_counts = defaultdict(int)
+sample_treatment = {}
+
+for delivery in target_deliveries:
+    with open(
+        os.path.join("..", "mgs-metadata", "deliveries", delivery, "metadata.tsv")
+    ) as f:
+        reader = csv.DictReader(f, delimiter="\t")
+        for row in reader:
+            sample = row["sample"]
+            if row.get("demultiplexed") == "False":
+                continue
+            date = datetime.strptime(row["date"], "%Y-%m-%d")
+            location = row["fine_location"]
+            treatment = row["notes"]
+            date_loc_read_counts[(date, location)] += read_counts.get(sample, 0)
+            sample_treatment[sample] = treatment
+            treatment_read_counts[treatment] += read_counts.get(sample, 0)
+
 
 
 # Create virus clade groupings
@@ -59,7 +93,7 @@ first_level_mapping = {
 
 # Initialize data structures
 samples = defaultdict(Counter)  # (date, location, pathogen) -> counts
-date_loc_sample_map = defaultdict(set)  # (date, location) -> set of samples
+treatment_samples = defaultdict(Counter) # (date, location, pathogen, treatment) -> counts
 
 # Process classified reads
 with open(os.path.join(validation_output_dir, "swabs-classified-all-reads.tsv")) as f:
@@ -72,14 +106,15 @@ with open(os.path.join(validation_output_dir, "swabs-classified-all-reads.tsv"))
         location = row["loc"]
         pathogen = row["genome_name"]
         sample = row["sample"]
-
-        date_loc_sample_map[(date, location)].add(sample)
+        treatment = sample_treatment[sample]
         if duplicate == "True":
             samples[(date, location, pathogen)]["non_dedup"] += 1
+            treatment_samples[(date, location, pathogen, treatment)]["non_dedup"] += 1
         else:
             samples[(date, location, pathogen)]["dedup"] += 1
             samples[(date, location, pathogen)]["non_dedup"] += 1
-
+            treatment_samples[(date, location, pathogen, treatment)]["non_dedup"] += 1
+            treatment_samples[(date, location, pathogen, treatment)]["dedup"] += 1
 
 # Process non-validated reads
 with open(os.path.join(validation_output_dir, "swabs-non-validated-reads.tsv")) as f:
@@ -93,20 +128,28 @@ with open(os.path.join(validation_output_dir, "swabs-non-validated-reads.tsv")) 
         taxid = row["taxid"]
         sample = row["sample_id"]
         pathogen = taxid_names[int(taxid)]
+        treatment = sample_treatment[sample]
 
-        date_loc_sample_map[(date, location)].add(sample)
         if duplicate == "True":
             samples[(date, location, pathogen)]["non_dedup"] += 1
+            treatment_samples[(date, location, pathogen, treatment)]["non_dedup"] += 1
         else:
             samples[(date, location, pathogen)]["dedup"] += 1
             samples[(date, location, pathogen)]["non_dedup"] += 1
+            treatment_samples[(date, location, pathogen, treatment)]["non_dedup"] += 1
+            treatment_samples[(date, location, pathogen, treatment)]["dedup"] += 1
 
-# Calculate total reads
+
+
+# Get reads for each grouping category
 for (date, location, pathogen), counts in samples.items():
-    n_reads = 0
-    for sample in date_loc_sample_map[(date, location)]:
-        n_reads += read_counts[sample]
+    n_reads = date_loc_read_counts[(date, location)]
     samples[(date, location, pathogen)]["all_reads"] = n_reads
+
+for (date, location, pathogen, treatment), counts in treatment_samples.items():
+    
+    n_reads = treatment_read_counts[treatment]
+    treatment_samples[(date, location, pathogen, treatment)]["all_reads"] = n_reads
 
 # Output results
 with open("swabs-ra-summary.tsv", "w") as outf:
@@ -126,3 +169,11 @@ with open("swabs-ra-summary.tsv", "w") as outf:
             data.get("dedup", 0),
             data["all_reads"],
         ])
+
+with open("swabs-ra-per-treatment-summary.tsv", "w") as outf:
+    writer = csv.writer(outf, delimiter="\t")
+    writer.writerow(["date", "location", "species", "pathogen", "treatment", "non_dedup", "dedup", "all_reads"])
+    for (date, location, pathogen, treatment), data in sorted(treatment_samples.items()):
+        species = first_level_mapping[pathogen]
+        date_str = date.strftime("%y%m%d")
+        writer.writerow([date_str, location, species, pathogen, treatment, data.get("non_dedup", 0), data.get("dedup", 0), data["all_reads"]])
