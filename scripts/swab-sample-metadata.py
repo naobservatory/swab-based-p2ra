74b8c5d76779cbbe7d5c39c18e79b36157e5eb4c
simonleandergrimm <simonleandergrimm@gmail.com>
2025-04-22 17:41:39 +0000

    Factored out delivery names.

--- scripts/swab-sample-metadata.py
+++ scripts/swab-sample-metadata.py
@@ -4,20 +4,15 @@ import csv
 from datetime import datetime
 import os
 from collections import Counter, defaultdict
-
+import json
 # Constants and paths
 tables_dir = "tables"
 os.makedirs(tables_dir, exist_ok=True)
-target_deliveries = [
-    "NAO-ONT-20250120-Zephyr8",
-    "NAO-ONT-20250127-Zephyr9",
-    "NAO-ONT-20250213-Zephyr10",
-    "NAO-ONT-20250213-Zephyr10-QC",
-    "NAO-ONT-20250220-Zephyr11",
-    "NAO-ONT-20250226-Zephyr10-QC2",
-    "NAO-ONT-20250313-Zephyr12",
-    "NAO-ONT-20250328-Zephyr12b"
-]
+
+with open("deliveries.json") as f:
+    deliveries = json.load(f)
+
+target_deliveries = deliveries["swab-deliveries"]
 
 
 # Load counts
