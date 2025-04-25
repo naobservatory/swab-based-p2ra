61ef4aef691be38c860a8b616036469c31e07773
simonleandergrimm <simonleandergrimm@gmail.com>
2025-04-25 11:53:12 +0000

    Revert "Disabling sorting."
    This reverts commit da9a3f4b5da30f2e3c84d6f24ff99432c67033bb.

--- scripts/swab-ra-tables.py
+++ scripts/swab-ra-tables.py
@@ -150,8 +150,8 @@ with open(os.path.join(TABLE_DIR, "swabs-ra-summary.tsv"), "w") as outf:
         "dedup_hv",
         "all_reads"
     ])
-    # Sort samples by date
-    sorted_samples = sorted(samples.items(), key=lambda x: x[0][0])
+    # Sort samples by date and pathogen
+    sorted_samples = sorted(samples.items(), key=lambda x: (x[0][0], x[0][2]))
     for (date, location, pathogen), data in sorted_samples:
         species = first_level_mapping(pathogen)
         group = second_level_mapping(species)
@@ -180,8 +180,8 @@ with open(os.path.join(TABLE_DIR, "swabs-ra-per-treatment-summary.tsv"), "w") as
         "dedup",
         "all_reads"
     ])
-    # Sort samples by date
-    sorted_samples = sorted(treatment_samples.items(), key=lambda x: x[0][0])
+    # Sort samples by date and pathogen
+    sorted_samples = sorted(treatment_samples.items(), key=lambda x: (x[0][0], x[0][2]))
 
     for (date, location, pathogen, treatment), data in sorted_samples:
         species = first_level_mapping(pathogen)
